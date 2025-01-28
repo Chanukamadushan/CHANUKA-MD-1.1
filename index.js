0966c94ee20346e5ec7d463c89f406b938ea43b0
Chanuka <Chanukamc5@gmail.com>
2025-01-28 21:58:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ async function connectToWA() {
   const prefix = config.PREFIX;
   //===========================
 
-  console.log("Connecting ❤️𝐑_𝐎_𝐁_𝐈_𝐍❤️");
+  console.log("Connecting 𝘾𝙃𝘼𝙉𝙐𝙆𝘼-𝙈𝘿");
   const { state, saveCreds } = await useMultiFileAuthState(
     __dirname + "/auth_info_baileys/"
   );
@@ -92,21 +92,21 @@ async function connectToWA() {
           require("./plugins/" + plugin);
         }
       });
-      console.log("❤️𝐑_𝐎_𝐁_𝐈_𝐍❤️ installed successful ✅");
-      console.log("❤️𝐑_𝐎_𝐁_𝐈_𝐍❤️ connected to whatsapp ✅");
+      console.log("𝘾𝙃𝘼𝙉𝙐𝙆𝘼-𝙈𝘿 installed successful ✅");
+      console.log("𝘾𝙃𝘼𝙉𝙐𝙆𝘼-𝙈𝘿 connected to whatsapp ✅");
 
-      let up = `❤️𝐑_𝐎_𝐁_𝐈_𝐍❤️ connected successful ✅`;
-      let up1 = `Hello Robin, I made bot successful`;
+      let up = `𝘾𝙃𝘼𝙉𝙐𝙆𝘼-𝙈𝘿 connected successful ✅`;
+      let up1 = `Hello CHANUKA-MD, I made bot successful`;
 
       robin.sendMessage(ownerNumber + "@s.whatsapp.net", {
         image: {
-          url: `https://raw.githubusercontent.com/Dark-Robin/Bot-Helper/refs/heads/main/autoimage/Bot%20robin%20cs.jpg`,
+          url: `https://pomf2.lain.la/f/vch2ef35.jpg`,
         },
         caption: up,
       });
-      robin.sendMessage("94705900209@s.whatsapp.net", {
+      robin.sendMessage("94741469245@s.whatsapp.net", {
         image: {
-          url: `https://raw.githubusercontent.com/Dark-Robin/Bot-Helper/refs/heads/main/autoimage/Bot%20robin%20cs.jpg`,
+          url: `https://pomf2.lain.la/f/vch2ef35.jpg`,
         },
         caption: up1,
       });
@@ -395,7 +395,7 @@ async function connectToWA() {
   });
 }
 app.get("/", (req, res) => {
-  res.send("hey, ❤️𝐑_𝐎_𝐁_𝐈_𝐍❤️ started✅");
+  res.send("𝙃𝙚𝙮, 𝘾𝙃𝘼𝙉𝙐𝙆𝘼-𝙈𝘿 𝙎𝙩𝙖𝙧𝙩𝙚𝙙✅");
 });
 app.listen(port, () =>
   console.log(`Server listening on port http://localhost:${port}`)
