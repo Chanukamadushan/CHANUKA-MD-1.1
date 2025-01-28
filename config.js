7b7433fcb720ee92fb51a55ebe89791d11afbd0b
Chanuka <Chanukamc5@gmail.com>
2025-01-28 20:51:22 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -1,30 +1,12 @@
+const fs = require("fs");
+if (fs.existsSync("config.env"))
+  require("dotenv").config({ path: "./config.env" });
 
-const fs = require('fs');
-if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
-
-function convertToBool(text, fault = 'true') {
-    return text === fault ? true : false;
+function convertToBool(text, fault = "true") {
+  return text === fault ? true : false;
 }
-
-//--------------------------𝗖𝗛𝗔𝗡𝗨𝗞𝗔-𝗠𝗗---------------------------------------
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "gLFAUYjK#27E_jm9W4E7jDZz2mPOIierzKQgdE7LzC5oQ2hjY678",
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", //TRUE OR FALSE
-MODE: process.env.MODE || "public",  // PRIVATE OR PUBLIC
-AUTO_VOICE: process.env.AUTO_VOICE || "true",
-AUTO_STICKER: process.env.AUTO_STICKER || "true",
-AUTO_REPLY: process.env.AUTO_REPLY || "true",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://pomf2.lain.la/f/p24bk9zh.jpg", // DO NOT CHANGE
-ALIVE_MSG: process.env.ALIVE_MSG || "ᴄʜᴀɴᴜᴋᴀ-ᴍᴅ ɪs ᴀʟɪᴠᴇ. ᴋᴇᴇᴘ sᴜᴘᴘᴏʀᴛɪɴɢ *ᴘᴏᴡᴇʀ Cᴏᴅᴇʀs❄️*\n\n> ᴅᴇᴠᴇʟᴏᴘᴇᴅ ʙʏ ᴄʜᴀɴᴜᴋᴀ-ᴍᴀᴅᴜꜱʜᴀɴ",
-ANTI_LINK: process.env.ANTI_LINK || "true",
-ANTI_BAD: process.env.ANTI_BAD || "true",
-PREFIX: process.env.PREFIX || ".", // .,!/?
-FAKE_RECORDING: process.env.FAKE_RECORDING || "false",
-AUTO_REACT: process.env.AUTO_REACT || "false",
-HEART_REACT: process.env.HEART_REACT || "false",
-OWNER_REACT: process.env.OWNER_REACT || "false",
-BOT_NAME: process.env.BOT_NAME || "『 ＣＨＡＮＵＫＡ - ＭＤ 』",
-OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
+  SESSION_ID: process.env.SESSION_ID || "Enter your session ID",
+  MONGODB: process.env.MONGODB || "Enter your mongoDB public URL",
+  OWNER_NUM: process.env.OWNER_NUM || "9470xxxxxxx",
 };
-
-//--------------MADE BY 𝗖𝗛𝗔𝗡𝗨𝗞𝗔 𝗠𝗔𝗫---------------->-
