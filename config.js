a823aa27e64460c17f2bac3a051bab136cb7ccad
Chanuka <Chanukamc5@gmail.com>
2025-01-30 02:02:31 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "Enter your session ID",
-  MONGODB: process.env.MONGODB || "Enter your mongoDB public URL",
+  SESSION_ID: process.env.SESSION_ID || "3ro2ARZL#5iQTaJFr2qrH5768W1fw0snylkFMfX8wH7gE3C50bVo",
+  MONGODB: process.env.MONGODB || "mongodb://mongo:ohfzuSToAeqwLtyJMlBkjUKBgGgCoDWf@roundhouse.proxy.rlwy.net:48583",
   OWNER_NUM: process.env.OWNER_NUM || "94741469245",
 };
